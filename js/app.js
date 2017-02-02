0768e97584492eec54cf09cd2cfb73911334d111
Ricardo Evans M <ricardoevansm@gmail.com>
2017-02-01 17:08:53 -0700

    Validacion js perfil de usuario con variables de session

--- js/app.js
+++ js/app.js
@@ -1,11 +1,26 @@
 
 function login(usuario, passwd)
 {
-    $.post("login.php", {txtUsuario:usuario, txtPass:passwd}, function(data)
+    $.post("login.php", {txtUsuario:usuario, txtPass:passwd}, function(response)
     {
+       var acceso = response.data.login;
+        if (acceso == true){
+            var usuario = response.data.usuario;
+            sessionStorage.usuario = usuario;
+            console.log(usuario);
+        } else if (acceso == false) {
+            $('#login-form-msg').html('Usuario o Contraseña Incorrecto');
+            $('#login-form-msg').removeClass('oculto');
+            $('#txtPass').val('');
+            console.warn('acceso incorrecto');
+        } else {
+            $('#login-form-msg').html('Error en la cadena de conexion');
+            $('#login-form-msg').removeClass('oculto');
+            console.error('error en la consulta');
+        }
+        
         return data;
     });
-    
 }
 $(document).ready(function()
 {
@@ -14,6 +29,20 @@ $('#login-button').click(function(event)
     event.preventDefault();
     var usuario = $('#txtUsuario').val();
     var passwd = $('#txtPass').val();
-    login(usuario,passwd);
+    if(usuario ==''){
+        //quitar la clase oculto del div de error de txtusuario 
+        $('#txtUsuario+div').removeClass('oculto');
+    } else {
+        $('#txtUsuario+div').addClass('oculto');
+    }
+    if (passwd = ''){
+        //quitar la clase oculto del div de error de txtPass
+        $('#txtPass+div').removeClass('oculto');
+    }else {
+        $('#txtPass+div').addClass('oculto');
+    }
+    if (usuario != '' && passwd !=''){
+        login(usuario,passwd);
+    }
 });
 });
